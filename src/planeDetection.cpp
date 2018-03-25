e456c86c55934609e724312daefc090196cf6c08
Joana <joancarvalhomota@ua.pt>
2018-03-25 22:07:17 +0100

    changes in segmentation and creation of launch file

--- src/planeDetection.cpp
+++ src/planeDetection.cpp
@@ -6,6 +6,8 @@
 #include <pcl/point_types.h>
 
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
+
 
 ros::Publisher pub;
 
@@ -19,10 +21,25 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
   // Convert to PCL data type
   pcl_conversions::toPCL(*cloud_msg, *cloud);
 
+  //Restringir área de trabalho 
+  pcl::PassThrough<pcl::PCLPointCloud2> pt;
+
+  pt.setInputCloud (cloudPtr);
+  pt.setFilterFieldName ("z");
+  pt.setFilterLimits (0, 0.7);
+  pcl::PCLPointCloud2::Ptr cloud_pt_kinect_ptr1 (new pcl::PCLPointCloud2);
+  pt.filter (*cloud_pt_kinect_ptr1);
+
+  pt.setInputCloud (cloud_pt_kinect_ptr1);
+  pt.setFilterFieldName ("y");
+  pt.setFilterLimits (-0.50, 0.10);
+  pcl::PCLPointCloud2::Ptr cloud_pt_kinect_ptr2 (new pcl::PCLPointCloud2);
+  pt.filter (*cloud_pt_kinect_ptr2);
+
   // Perform the actual filtering
   pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-  sor.setInputCloud (cloudPtr);
-  sor.setLeafSize (0.5, 0.5, 0.5);
+  sor.setInputCloud (cloud_pt_kinect_ptr2);
+  sor.setLeafSize (0.005f, 0.005f, 0.005f);
   sor.filter (cloud_filtered);
 
   // Convert to ROS data type
@@ -36,7 +53,7 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 int main (int argc, char** argv)
 {
   // Initialize ROS
-  ros::init (argc, argv, "bin-picking");
+  ros::init (argc, argv, "bin_picking");
   ros::NodeHandle nh;
 
   // Create a ROS subscriber for the input point cloud
