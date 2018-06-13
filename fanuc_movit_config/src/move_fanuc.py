29eea39878d7c3c03dbfd70c39fb332a92785930
Joana <joancarvalhomota@ua.pt>
2018-06-13 12:45:39 +0100

    move robot

--- fanuc_movit_config/src/move_fanuc.py
+++ fanuc_movit_config/src/move_fanuc.py
@@ -8,7 +8,9 @@ import moveit_msgs.msg
 import geometry_msgs.msg
 from math import pi
 import numpy as np
-import tf
+# import tf
+from tf.msg import tfMessage
+from tf.transformations import quaternion_from_euler
 
 # initialize moveit_commander and rospy
 print "============ Starting movement setup"
@@ -51,7 +53,7 @@ print "============"
 print "============ Generating plan 1"
 pose_target = geometry_msgs.msg.Pose()
 
-pose_target.orientation.x = 0.0
+pose_target.orientation.x = 0.0 
 pose_target.orientation.y = 1
 pose_target.orientation.z = 0.0
 pose_target.orientation.w = 1.0
@@ -70,7 +72,64 @@ display_trajectory = moveit_msgs.msg.DisplayTrajectory()
 
 display_trajectory.trajectory_start = robot.get_current_state()
 display_trajectory.trajectory.append(plan1)
-display_trajectory_publisher.publish(display_trajectory);
+display_trajectory_publisher.publish(display_trajectory)
 
 print "============ Waiting while plan1 is visualized (again)..."
 rospy.sleep(5)
+
+# Points for a clean path
+waypoints = []
+
+# start with the current pose
+waypoints.append(group.get_current_pose().pose)
+
+# first orient gripper and move forward (+x)
+wpose = geometry_msgs.msg.Pose()
+wpose.orientation.w = 1.0
+wpose.position.x = waypoints[0].position.x + 0.1
+wpose.position.y = waypoints[0].position.y
+wpose.position.z = waypoints[0].position.z
+waypoints.append(copy.deepcopy(wpose))
+
+# second move down
+wpose.position.z -= 0.10
+waypoints.append(copy.deepcopy(wpose))
+
+# third move to the side
+wpose.position.y += 0.05
+waypoints.append(copy.deepcopy(wpose))
+
+(plan3, fraction) = group.compute_cartesian_path(
+                             waypoints,   # waypoints to follow
+                             0.01,        # eef_step
+                             0.0)         # jump_threshold
+
+print "============ Waiting while RVIZ displays plan3..."
+rospy.sleep(5)
+
+if fraction == 1.0:
+    print 'planning was successful'
+    raw_input()
+    group.execute(plan3)
+else:
+    print 'planning was not successfull'
+print fraction
+
+
+# q = tf.transformations.quaternion_from_euler(yaw, pitch, roll, 'rzyx')
+
+# q = quaternion_from_euler(roll, pitch, yaw)
+
+# print "The quaternion representation is %s %s %s %s." % (q[0], q[1], q[2], q[3])
+
+# cr = cos(roll * 0.5)
+# sr = sin(roll * 0.5)
+# cp = cos(pitch * 0.5)
+# sp = sin(pitch * 0.5)
+# cy = cos(yaw * 0.5)
+# sy = sin(yaw * 0.5)
+
+# q.w() = cr * cy * cp + sr * sy * sp
+# q.z() = sr * cy * cp - cr * sy * sp
+# q.x() = cr * sy * cp - sr * cy * sp
+# q.y() = cr * cy * sp + sr * sy * cp
